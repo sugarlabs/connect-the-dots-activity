b2740a3f88198664f133784706959531aa007120
sachi-d <sachithradangalla.13@cse.mrt.ac.lk>
2016-12-24 09:48:02 +0530

    add generic label alignment

--- js/activity.js
+++ js/activity.js
@@ -4,39 +4,31 @@ define(function (require) {
     require("easel");
     require("handlebars")
     var shapes = require("activity/shapes")
-
-    // Manipulate the DOM only when it is ready.
+        // Manipulate the DOM only when it is ready.
     require(['domReady!'], function (doc) {
-
         // Initialize the activity.
         activity.setup();
-
         // Colorize the activity icon.
         var activityButton = document.getElementById("activity-button");
         activity.getXOColor(function (error, colors) {
             icon.colorize(activityButton, colors);
         });
-
         var newButton = document.getElementById("new-button");
         newButton.onclick = function () {
-            new_positions();
-        }
-
-        // Make the activity stop with the stop button.
+                new_positions();
+            }
+            // Make the activity stop with the stop button.
         var stopButton = document.getElementById("stop-button");
         stopButton.addEventListener('click', function (e) {
             activity.close();
         });
-
         // Then create a list of the label elements
         nlabels = [];
         for (i = 0; i < 21; i++) {
             nlabels[i] = document.getElementById("n" + i.toString());
         }
-
         // Stage is an Easel construct
         var canvas, stage;
-
         // The display object currently under the mouse, or being dragged
         var mouseTarget;
         // Indicates whether we are currently in a drag operation
@@ -51,19 +43,15 @@ define(function (require) {
         var stroke;
         var colors;
         var index;
-
         var imagepos = new Array();
         var myimages = new Array();
         var bitmaps = new Array();
         var bitmapLabels = new Array();
         var pen_bitmap;
-
         var Star = "images/star.svg";
         var Dot = "images/dot.svg";
         var Pen = "images/pen.svg";
-
         var shape = -1;
-
         // Get things started
         init();
 
@@ -74,25 +62,19 @@ define(function (require) {
             document.getElementById("loader").className = "loader";
             // Create the stage and point it to the canvas:
             canvas = document.getElementById("myCanvas");
-
             index = 0;
             colors = ["#D0033C", "#01A4A9", "#E9510E", "#CCCE01", "#FDC415", "#E1338C"];
             oldPt = new createjs.Point(400, 300);
             midPt = oldPt;
             oldMidPt = oldPt;
-
             // Check to see if we are running in a browser with touch support
             stage = new createjs.Stage(canvas);
-
             // Enable touch interactions if supported on the current device:
             createjs.Touch.enable(stage);
-
             // Keep tracking the mouse even when it leaves the canvas
             stage.mouseMoveOutside = true;
-
             // Enabled mouse over and mouse out events
             stage.enableMouseOver(10);
-
             // Load the source images: a dot, a star and a turtle
             for (i = 0; i < nlabels.length; i++) {
                 imagepos[i] = [-100, -100];
@@ -102,16 +84,15 @@ define(function (require) {
                 myimages[i].dataId = i;
                 if (i == 0) {
                     myimages[i].src = Star;
-                } else {
+                }
+                else {
                     myimages[i].src = Dot;
                 }
                 myimages[i].onload = handleImageLoad;
             }
-
             pen = new Image();
             pen.src = Pen;
             pen.onload = handlePenLoad;
-
             // Create a drawing canvas
             drawingCanvas = new createjs.Shape();
             stage.addChild(drawingCanvas);
@@ -130,7 +111,6 @@ define(function (require) {
             var bitmap, bitText;
             var container = new createjs.Container();
             stage.addChild(container);
-
             // Create a shape that represents the center of the icon:
             var hitArea = new createjs.Shape();
             hitArea.graphics.beginFill("#FFF").drawEllipse(-11, -14, 24, 18);
@@ -138,39 +118,32 @@ define(function (require) {
             // of the target (bitmap instances):
             hitArea.x = imgW / 2;
             hitArea.y = imgH / 2;
-
             i = myimages.indexOf(image)
                 // Create and populate the screen with number icons.
             bitmap = new createjs.Bitmap(image);
             bitmaps[i] = bitmap // Save now so we can reposition later.
-
             bitText = new createjs.Text(image.dataId.toString(), "bold 20px Arial", "#000");
             bitmapLabels[i] = bitText;
             container.addChild(bitmap);
             container.addChild(bitText);
-            var labelX = imagepos[i][0],
-                labelY = imagepos[i][1];
-            bitmap.x = labelX,
-                bitmap.y = labelY;
-            bitText.x = labelX,
-                bitText.y = labelY;
+            var labelX = imagepos[i][0]
+                , labelY = imagepos[i][1];
+            bitmap.x = labelX, bitmap.y = labelY;
+            bitText.x = labelX, bitText.y = labelY;
             bitmap.regX = imgW / 2 | 0;
             bitmap.regY = imgH / 2 | 0;
             if (i == 0) {
                 bitmap.scaleX = bitmap.scaleY = bitmap.scale = 0.5
-            } else {
+            }
+            else {
                 bitmap.scaleX = bitmap.scaleY = bitmap.scale = 1.5
             }
             bitmap.name = "bmp_" + i;
-
             bitmap.cursor = "pointer";
-
             // Eventually, we can check this to make sure the number
             // has been touched.
             bitmap.hitArea = hitArea;
-
             (function (target) {})(bitmap);
-
             document.getElementById("loader").className = "";
             createjs.Ticker.addEventListener("tick", tick);
         }
@@ -189,7 +162,6 @@ define(function (require) {
             // of the target (bitmap instances):
             hitArea.x = imgW / 2;
             hitArea.y = imgH / 2;
-
             // Create a pen
             bitmap = new createjs.Bitmap(image);
             pen_bitmap = bitmap
@@ -200,36 +172,28 @@ define(function (require) {
             bitmap.regY = imgH / 2 | 0;
             bitmap.scaleX = bitmap.scaleY = bitmap.scale = 1
             bitmap.name = "bmp_pen";
-
             bitmap.cursor = "pointer";
-
             // Assign the hitArea to bitmap to use it for hit tests:
             bitmap.hitArea = hitArea;
-
             // Wrapper function to provide scope for the event handlers:
             (function (target) {
                 bitmap.onPress = function (evt) {
                     // Bump the target in front of its siblings:
                     container.addChild(target);
                     var offset = {
-                        x: target.x - evt.stageX,
-                        y: target.y - evt.stageY
+                        x: target.x - evt.stageX
+                        , y: target.y - evt.stageY
                     };
-
                     evt.onMouseMove = function (ev) {
                         target.x = ev.stageX + offset.x;
                         target.y = ev.stageY + offset.y;
                         // Indicate that the stage should be updated
                         // on the next tick:
                         update = true;
-                        var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1,
-                            oldPt.y + stage.mouseY >> 1);
-
+                        var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
                         drawingCanvas.graphics.setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
-
                         oldPt.x = stage.mouseX;
                         oldPt.y = stage.mouseY;
-
                         oldMidPt.x = midPt.x;
                         oldMidPt.y = midPt.y;
                     }
@@ -247,7 +211,6 @@ define(function (require) {
                     update = true;
                 }
             })(bitmap);
-
             document.getElementById("loader").className = "";
             createjs.Ticker.addEventListener("tick", tick);
         }
@@ -266,43 +229,39 @@ define(function (require) {
                 shape = 0
                 return
             }
-
             for (i = 0; i < bitmaps.length; i++) {
                 if (shape < shapes.length) {
                     if (i < shapes[shape].length) {
-                        var shapeX = shapes[shape][i][0],
-                            shapeY = shapes[shape][i][1];
+                        var shapeX = shapes[shape][i][0]
+                            , shapeY = shapes[shape][i][1]
+                            , fontSize = 6
+                            , ovrhdX = i.toString().length * fontSize
+                            , ovrhdY = fontSize + 4;
                         bitmaps[i].x = shapeX;
                         bitmaps[i].y = shapeY;
-                        bitmapLabels[i].x = shapeX - 5;
-                        bitmapLabels[i].y = shapeY - 7;
-                        if (i > 9) {
-                            bitmapLabels[i].x = shapeX - 11;
-                        }
-                    } else {
+                        bitmapLabels[i].x = shapeX - ovrhdX;
+                        bitmapLabels[i].y = shapeY - ovrhdY;
+                    }
+                    else {
                         var outRange = -100;
                         bitmaps[i].x = outRange;
                         bitmaps[i].y = outRange;
                         bitmapLabels[i].x = outRange;
                         bitmapLabels[i].y = outRange;
                     }
-                } else {
+                }
+                else {
                     bitmaps[i].x = canvas.width * Math.random() | 0;
                     bitmaps[i].y = canvas.height * Math.random() | 0;
                     bitmapLabels[i].x = canvas.width * Math.random() | 0;
                     bitmapLabels[i].y = canvas.height * Math.random() | 0;
                 }
             }
-
             pen_bitmap.x = bitmaps[0].x;
             pen_bitmap.y = bitmaps[0].y;
-
             drawingCanvas.graphics.clear();
-
             update = true;
             shape = shape + 1;
         }
-
     });
-
 });
